2f39b755f2c499da488377ad56a9783417ca9ea2
haleyjmac <haleyjmacmillan@outlook.com>
2026-07-24 22:42:08 -0400

    added organization name, project name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'HaleyMacMillan', // Usually your GitHub org/user name.
+  projectName: 'assignment3-docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
