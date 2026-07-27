41cf68091495bba543b04daa93667833d769734b
haleyjmac <haleyjmacmillan@outlook.com>
2026-07-26 20:51:50 -0400

    Added deploy.yml file to deploy on github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,14 +23,16 @@ const config = {
   url: 'https://haleyjmac.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/assignment3-docusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'haleyjmac', // Usually your GitHub org/user name.
   projectName: 'assignment3-docusaurus', // Usually your repo name.
   deploymentBranch:'gh-pages', //Branch for deployment
-  
+  trailingSlash: false,
+  //...
+
 
   onBrokenLinks: 'throw',
 
